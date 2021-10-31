60ff22f4b681b769bf534d6ff3bc2d03bd729ce9
marlavidal <marla.mvrl@gmail.com>
2021-10-30 20:43:13 -0700

    atualizando o github

---
+++ Desafios/01.DesafiosIniciaisJavaScript/Tabuada.js
@@ -0,0 +1,34 @@
+// Tabuada
+
+/* Leia 1 valor inteiro N (2 < N < 1000). A seguir, mostre a tabuada de N:      
+1 x N = N      2 x N = 2N        ...       10 x N = 10N
+Entrada
+A entrada contém um valor inteiro N (2 < N < 1000).
+Saída
+Imprima a tabuada de N, conforme o exemplo fornecido.
+ 
+Exemplo de Entrada	Exemplo de Saída
+140
+1 x 140 = 140
+2 x 140 = 280
+3 x 140 = 420
+4 x 140 = 560
+5 x 140 = 700
+6 x 140 = 840
+7 x 140 = 980
+8 x 140 = 1120
+9 x 140 = 1260
+10 x 140 = 1400 */
+
+let N = gets();
+
+print("1 x " + N + " = " + (N * 1));
+print("2 x " + N + " = " + (N * 2));
+print("3 x " + N + " = " + (N * 3));
+print("4 x " + N + " = " + (N * 4  ));
+print("5 x " + N  + " = " +  ( N * 5  ));
+print("6 x " + N    + " = " + ( N * 6 ));
+print("7 x " + N    + " = " + ( N * 7 ));
+print("8 x " + N    + " = " + ( N * 8 ));
+print("9 x " + N    + " = " + ( N * 9   ));
+print("10 x " + N    + " = " + ( N *10  ));
---
+++ Projetos/03.RecriandoOFamosoJogoDODinossauroSemInternet/README.md
@@ -0,0 +1,16 @@
+# "Recriando o famoso jogo do dinossauro sem internet" 
+
+
+Neste projeto você terá o desafio de criar uma cópia do jogo do dinossauro, famoso no Chrome por nos entreter quando estamos sem conexão. Nele iremos utilizar HTML, CSS e JavaScript, abordando de maneira simples diversos conceitos introdutórios importantes para programação na web como tags básicas de HTML, manipulação de eventos, funções e manipulação de elementos HTML usando JavaScript, estilização e animações básicas com CSS.
+
+Sobre
+
+Projeto desenvolvido durante o Bootcamp HTML Web Developer promovido pela DIO
+
+Ministrado por Celso Henrique, Engineering Lead, Creditas
+
+🚀 Ferramentas
+HTML
+CSS
+JavaScript
+
